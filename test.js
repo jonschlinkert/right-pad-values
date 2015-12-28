3c7f5a1d5aa6c8255615e53782e1f778a097cfff
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-27 22:14:17 -0500

    use assert

--- test.js
+++ test.js
@@ -7,16 +7,16 @@
 
 'use strict';
 
-/* deps:mocha */
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var pad = require('./');
 
 var arr = [{a: 'b'}, {a: 'bb'}, {a: 'bbbb'}, {a: 'bbb'}, {a: 'bb'}];
 
-describe('right pad values', function () {
-  describe('array:', function () {
-    it('should right-pad the values for the given property:', function () {
-      pad(arr, 'a').should.eql([
+describe('right pad values', function() {
+  describe('array:', function() {
+    it('should right-pad the values for the given property:', function() {
+      assert.deepEqual(pad(arr, 'a'), [
         {a: 'b   '},
         {a: 'bb  '},
         {a: 'bbbb'},
@@ -26,9 +26,9 @@ describe('right pad values', function () {
     });
   });
 
-  describe('object:', function () {
-    it('should right-pad the values of an object:', function () {
-      pad({a: 'b', c: 'dddddd', e: 'fff', g: 'hhhhh'}).should.eql({
+  describe('object:', function() {
+    it('should right-pad the values of an object:', function() {
+      assert.deepEqual(pad({a: 'b', c: 'dddddd', e: 'fff', g: 'hhhhh'}), {
         a: 'b     ',
         c: 'dddddd',
         e: 'fff   ',
@@ -37,8 +37,8 @@ describe('right pad values', function () {
     });
   });
 
-  describe('objects:', function () {
-    it.only('should right-pad values of a specific object property:', function () {
+  describe('objects:', function() {
+    it('should right-pad values of a specific object property:', function() {
       var res = pad('foo', {
         a: {
           foo: 'a',
@@ -54,26 +54,30 @@ describe('right pad values', function () {
         }
       });
 
-      // res.should.eql({
-      //   a: {
-      //     foo: 'a',
-      //     bar: 'z'
-      //   },
-      //   b: {
-      //     foo: 'aaaaaaa',
-      //     bar: 'z'
-      //   },
-      //   c: {
-      //     foo: 'aaa',
-      //     bar: 'z'
-      //   }
-      // });
+      assert.deepEqual(res, {
+        a: {
+          foo: 'a      ',
+          bar: 'z'
+        },
+        b: {
+          foo: 'aaaaaaa',
+          bar: 'z'
+        },
+        c: {
+          foo: 'aaa    ',
+          bar: 'z'
+        }
+      });
     });
   });
 
-  it('should throw an error when an array is not passed:', function () {
-    (function () {
+  it('should throw an error when an array is not passed:', function(cb) {
+    try {
       pad();
-    }).should.throw('right-pad-values expects an object or array.');
+      cb(new Error('expected an error'));
+    } catch (err) {
+      assert.equal(err.message, 'right-pad-values expects an object or array.');
+      cb();
+    }
   });
 });
